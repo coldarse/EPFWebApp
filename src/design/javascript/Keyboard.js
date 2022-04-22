5774d6c12ff99135ce15a96b15a3dc874a2d1f57
coldarse <37390180+coldarse@users.noreply.github.com>
2022-04-22 14:46:38 +0800

    Set Address 1-3 max length of 40

--- src/design/javascript/Keyboard.js
+++ src/design/javascript/Keyboard.js
@@ -333,11 +333,14 @@ const Keyboard = {
         else if(element.tagname.toLowerCase().includes('compname')){
             max_chars = 255;
         }
-        else if(element.tagname.toLowerCase().includes('add1')){
-            max_chars = 255;
+        else if(element.tagname.toLowerCase().includes('address_1')){
+            max_chars = 40;
         }
-        else if(element.tagname.toLowerCase().includes('add2')){
-            max_chars = 255;
+        else if(element.tagname.toLowerCase().includes('address_2')){
+            max_chars = 40;
+        }
+        else if(element.tagname.toLowerCase().includes('address_3')){
+            max_chars = 40;
         }
         else if(element.tagname.toLowerCase().includes('emailadd')){
             max_chars = 60;
